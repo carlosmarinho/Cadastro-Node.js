b0f15a99f7c6585e9ae8c897213476ad27312bfd
rails <carlos.marinho@umbarato.com.br>
2016-10-22 17:02:20 -0200

    feito a busca e iniciado a troca de senha

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -9,7 +9,20 @@ module.exports = {
 	index: function (req, res) {
 		if( req.session.authenticated && req.session.passport.isAdmin == '1' )
 		{
-	    User.find(function foundUser (err, users) {
+			console.log(req.param('Busca'))
+			if(req.param('Busca') == undefined)
+			{
+				busca = {};
+			}
+			else
+			{
+				busca = req.param('Busca')
+				busca = {name: { like: busca['name']+'%'}};
+			}
+
+			console.log(busca);
+			
+	    User.find(busca).exec(function foundUser (err, users) {
 	      if(err) return res.redirect('/user/new');
 	      res.view({
 	        users: users
@@ -37,6 +50,33 @@ module.exports = {
 			  }
 			})
     	
+  },
+
+	password: function (req, res) {
+
+			console.log(req.param('id'))
+      Passport.findOne({User: req.param('id')}).exec(function (err, passport){
+      	console.log(err);
+      	console.log(passport);
+      	/*if(req.method=="POST" && req.param("User",null)!=null)
+      	{
+      		var u=req.param("User",null);
+      		console
+        	user.username	= u.username;
+        	user.name			= u.name;
+        	user.email		= u.email;
+
+        	user.save(function(err){
+        		if (err) {
+        			console.log(err)
+	          	res.send("Error");
+		        }else {
+		          res.redirect( 'user/show/'+user.id);
+		        }
+        	})
+      	}*/
+      });
+      res.view(); 
   },
 
   update: function (req, res) {
